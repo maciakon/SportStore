9121e868ec722550d282ceb41569492888abec57
maciakon <k.macialek@gmail.com>
2018-06-16 08:58:33 +0200

    Order view and controller added.

---
+++ src/SportStore/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
