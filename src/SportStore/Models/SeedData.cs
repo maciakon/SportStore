349b668e22d99b4fb795e02a832abbf7a0c35b29
maciakon <k.macialek@gmail.com>
2018-06-26 18:51:15 +0200

    Seed data corrected for production.

--- src/SportStore/Models/SeedData.cs
+++ src/SportStore/Models/SeedData.cs
@@ -16,15 +16,15 @@ namespace SportStore.Models
             if(context.Products.Any()) return;
 
             context.Products.AddRange(
-                new Product { ProductId = 1, Name = "Kayak", Description = "A boat for one person", Category="Watersports", Price= 275},
-                new Product { ProductId = 2, Name = "Lifejacket", Description = "Protective and fashionable", Category="Watersports", Price = 48.95m},
-                new Product { ProductId = 3, Name = "Soccer ball", Description = "FIFA-approved size and weight", Category="Soccer", Price = 19.50m},
-                new Product { ProductId = 4, Name = "Corner flags", Description = "Give your playing field a professional touch", Category="Soccer", Price = 19.50m},
-                new Product { ProductId = 5, Name = "Stadium", Description = "Flat-packed, 35.000 seats stadium", Category="Soccer", Price = 79500},
-                new Product { ProductId = 6, Name = "Thinking cap", Description = "Improve brain efficiency by 75%", Category="Chess", Price = 16},
-                new Product { ProductId = 7, Name = "Unsteady chair", Description = "Secretly gives your opponnen a disadvantage", Category="Chess", Price = 29.95m},
-                new Product { ProductId = 8, Name = "Human chess board", Description = "A fun game for the family", Category="Chess", Price = 75},
-                new Product { ProductId = 9, Name = "Bling-Bling King", Description = "Gold-plated, diamond-studded King", Category="Chess", Price = 1200}
+                new Product { Name = "Kayak", Description = "A boat for one person", Category="Watersports", Price= 275},
+                new Product { Name = "Lifejacket", Description = "Protective and fashionable", Category="Watersports", Price = 48.95m},
+                new Product { Name = "Soccer ball", Description = "FIFA-approved size and weight", Category="Soccer", Price = 19.50m},
+                new Product { Name = "Corner flags", Description = "Give your playing field a professional touch", Category="Soccer", Price = 19.50m},
+                new Product { Name = "Stadium", Description = "Flat-packed, 35.000 seats stadium", Category="Soccer", Price = 79500},
+                new Product { Name = "Thinking cap", Description = "Improve brain efficiency by 75%", Category="Chess", Price = 16},
+                new Product { Name = "Unsteady chair", Description = "Secretly gives your opponnen a disadvantage", Category="Chess", Price = 29.95m},
+                new Product { Name = "Human chess board", Description = "A fun game for the family", Category="Chess", Price = 75},
+                new Product { Name = "Bling-Bling King", Description = "Gold-plated, diamond-studded King", Category="Chess", Price = 1200}
             );
             context.SaveChanges();
         }
